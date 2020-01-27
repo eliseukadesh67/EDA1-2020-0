0483eb12b5803647edd2d3a73fba556e7fbaac09
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-01-27 10:01:24 -0300

    Adicionando a correção da questão da atividade 3.

--- Atividade-3/par_carac.c
+++ Atividade-3/par_carac.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
-int cont_par(char *str)
+int cont_par(char *str, int cont)
 {
-    int cont = 0;
+    if (*str)
+    {
+
+        if (*str == *(str + 2))
+        {
+            cont++;
+        }
+
+        return cont_par(str + 1, cont);
+    }
+    else
+    {
+        return cont;
+    }
 }
 
 int main()
 {
     char str[200];
     scanf("%s", str);
-    printf("%d\n", cont_par(str));
+    printf("%d\n", cont_par(str, 0));
     return 0;
 }
--- Atividade-3/rumo_9s.c
+++ Atividade-3/rumo_9s.c
@@ -14,45 +14,11 @@ long long int soma(unsigned long long int n)
         return ans;
     }
 }
-
-int div(unsigned long long int n)
-{
-    int ans = 1;
-    if (n == 9)
-    {
-        return ans;
-    }
-    else if (n < 9)
-    {
-
-        ans = 0;
-        return 0;
-    }
-    else
-    {
-        ans += div(soma(n));
-        return ans;
-    }
-}
-
 int main()
 {
     char n[1000];
-    while (n != "0")
-    {
-        scanf("%s", n);
-        if (n == "0")
-        {
-            break;
-        }
-        if (div(atoi(n)) - 1)
-        {
-            printf("%s is a multiple of 9 and has 9-degree %d.\n", n, div(n));
-        }
-        else
-        {
-            printf("%s is not a multiple of 9.\n", n);
-        }
-    }
+    scanf("%s", n);
+
+    printf("%s\n", n);
     return 0;
 }
