c3359ca1b5e33d5514d6590342ddda0437a0621c
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-02-11 10:17:00 -0300

    Melhorando a questão da Atividade 5.

--- Atividade 5/enfileira_filacircular.c
+++ Atividade 5/enfileira_filacircular.c
@@ -8,20 +8,82 @@ typedef struct fila
 
 int enfileira(fila *f, int x)
 {
-    if (f->p < f->u)
+    if (((f->u + 1) % (f->N)) == f->p)
     {
-        //Redimensiona
+        if (f->u < f->p)
+        {
+            if (f->u < (f->N - f->p))
+            {
+                f->N *= 2;
+                f->dados = realloc(f->dados, f->N * sizeof(int));
+                if (f->dados == NULL)
+                {
+                    return 0;
+                }
+                int cont = f->N / 2;
+                while (f->u)
+                {
+                    f->u--;
+                    f->dados[cont] = f->dados[f->u];
+                    cont++;
+                }
+
+                f->u = cont;
+            }
+            else if (f->u > (f->N - f->p))
+            {
+                f->N *= 2;
+                f->dados = realloc(f->dados, f->N * sizeof(int));
+                if (f->dados == NULL)
+                {
+                    return 0;
+                }
+                int cont_fim = f->N - 1;
+                int cont_init = (f->N / 2) - 1;
+
+                while (cont_init >= f->p)
+                {
+                    f->dados[cont_fim] = f->dados[cont_init];
+                    cont_init--;
+                    cont_fim--;
+                }
+
+                f->p = cont_fim;
+            }
+        }
     }
-    else if (f->u < f->p)
+
+    f->dados[f->u] = x;
+    f->u++;
+    if (f->u == f->N)
     {
-        if (f->u < f->N - f->p)
-        {
-        }
-        else if (f->u > f->N - f->p)
-        {
-        }
-        else
-        {
-        }
+        f->u = 0;
     }
+
+    return 1;
 }
+
+/* int main()
+{
+    fila *f = malloc(sizeof(fila));
+    f->N = 5;
+    f->u = f->p = 0;
+    f->dados = malloc(f->N * sizeof(int));
+    printf("%d\n", enfileira(f, 1));
+    printf("%d\n", enfileira(f, 2));
+    printf("%d\n", enfileira(f, 3));
+    printf("%d\n", enfileira(f, 4));
+    f->p++;
+    f->p++;
+    f->p++;
+    printf("%d\n", enfileira(f, 5));
+    f->p++;
+    printf("%d\n", enfileira(f, 6));
+    printf("%d\n", enfileira(f, 7));
+    printf("%d\n", enfileira(f, 8));
+    printf("%d\n", enfileira(f, 9));
+    printf("Primeiro : %d\n", f->p);
+    printf("Ultimo : %d\n", f->u);
+    free(f->dados);
+    free(f);
+} */
